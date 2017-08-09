07c82e8f9b32618b1942d68875a4103e18a63c6b
Gabriel Martins da Silva <gms.1997@hotmail.com>
2017-08-09 14:04:05 -0300

    Padding issue corrected

--- LegislativoTeste/app/src/main/java/zimmermann/larissa/legislativoteste/ProposicaoAdapter.java
+++ LegislativoTeste/app/src/main/java/zimmermann/larissa/legislativoteste/ProposicaoAdapter.java
@@ -1,9 +1,7 @@
 package zimmermann.larissa.legislativoteste;
 
 import android.content.Context;
-import android.support.v4.provider.DocumentFile;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +16,6 @@ import com.bluejamesbond.text.DocumentView;
 
 import java.util.List;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import zimmermann.larissa.legislativoteste.rest.StringUtils;
-
 public class ProposicaoAdapter extends RecyclerView.Adapter<ProposicaoAdapter.ProposicaoViewHolder> {
 
     private List<Proposicao> props;
@@ -34,15 +25,17 @@ public class ProposicaoAdapter extends RecyclerView.Adapter<ProposicaoAdapter.Pr
 
     public static class ProposicaoViewHolder extends RecyclerView.ViewHolder {
         LinearLayout propsLayout;
-        TextView dadosLabel;
-        TextView dados;
+        TextView idLabel, anoLabel;
+        TextView id, ano;
         DocumentView ementa;
 
         public ProposicaoViewHolder(View v) {
             super(v);
             propsLayout = (LinearLayout) v.findViewById(R.id.proposicao_layout);
-            dadosLabel = (TextView) v.findViewById(R.id.dadosLabel);
-            dados = (TextView) v.findViewById(R.id.dados);
+            idLabel = (TextView) v.findViewById(R.id.idLabel);
+            id = (TextView) v.findViewById(R.id.id);
+            anoLabel = (TextView) v.findViewById(R.id.anoLabel);
+            ano = (TextView) v.findViewById(R.id.ano);
             ementa = (DocumentView) v.findViewById(R.id.ementa);
         }
     }
@@ -56,18 +49,23 @@ public class ProposicaoAdapter extends RecyclerView.Adapter<ProposicaoAdapter.Pr
     @Override
     public ProposicaoAdapter.ProposicaoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout, parent, false);
-        return new ProposicaoViewHolder(view);
+        ProposicaoViewHolder holder = new ProposicaoViewHolder(view);
+        return holder;
     }
 
 
     @Override
     public void onBindViewHolder(final ProposicaoViewHolder holder, final int position) {
 
-        holder.ementa.setText("Ementa: " + props.get(position).getEmenta().toUpperCase());
+        holder.ementa.setText(null);
+        holder.idLabel.setText(null);
+        holder.id.setText(null);
 
-        holder.dadosLabel.setText("Número: ");
-        holder.dados.setText(props.get(position).getId() +
-                             " - Ano: " + props.get(position).getAno());
+        holder.ementa.setText("Ementa: " + props.get(position).getEmenta().toUpperCase());
+        holder.idLabel.setText("Código: ");
+        holder.id.setText(String.valueOf(props.get(position).getId()));
+        holder.anoLabel.setText("Ano: ");
+        holder.ano.setText(String.valueOf(props.get(position).getAno()));
 
     }
 
