8183cc39e00434b0207497184d7184e4bdfa0ca6
Larissa Cardoso Zimmermann <larissacazi@hotmail.com>
2017-08-14 10:51:37 -0300

    Organize Main Menu.

--- LegislativoApp/app/src/main/java/zimmermann/larissa/legislativoapp/MainActivity.java
+++ LegislativoApp/app/src/main/java/zimmermann/larissa/legislativoapp/MainActivity.java
@@ -94,11 +94,11 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_prop) {
             loadProps();
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_prop_situacao) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_prop_ano) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_tutorial) {
 
         } else if (id == R.id.nav_share) {
 
